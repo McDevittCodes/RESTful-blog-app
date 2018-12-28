72c299be3d140022fe6f747c98c5a47c9644c02e
Linda Allen <lindaallenx@gmail.com>
2018-12-28 23:37:26 +0000

    create schema/model and index route

--- app.js
+++ app.js
@@ -9,10 +9,35 @@ app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
 
-//title
-//image
-//body
-//created
+//MONGOOSE/MODEL CONFIG
+var blogSchema = new mongoose.Schema({
+    title: String,
+    image: String,
+    body: String,
+    created: {type: Date, default: Date.now}
+});
+var Blog = mongoose.model("Blog", blogSchema);
+
+// Blog.create({
+//     title: "Test Blog",
+//     image: "https://images.unsplash.com/photo-1508280756091-9bdd7ef1f463?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1090&q=80",
+//     body: "HELLO THIS IS A BLOG POST"
+// });
+
+//RESTFUL ROUTES
+app.get("/", function(req, res){
+   res.redirect("/blogs"); 
+});
+
+app.get("/blogs", function(req, res){
+    Blog.find({}, function(err, blogs){
+        if(err){
+            console.log("ERROR");
+        } else {
+            res.render("index", {blogs:blogs})
+        }
+    });
+});
 
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("Server is running");
