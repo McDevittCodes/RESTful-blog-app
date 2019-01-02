3aaf00611510976d1f4b604d40fddad3e56f5d76
Linda Allen <lindaallenx@gmail.com>
2019-01-02 19:50:03 +0000

    add sanitizer to create and update route

--- app.js
+++ app.js
@@ -2,13 +2,15 @@ var express = require("express"),
 app = express(),
 bodyParser = require("body-parser"),
 mongoose = require("mongoose"),
-methodOverride = require("method-override");
+methodOverride = require("method-override"),
+expressSanitizer = require("express-sanitizer");
 
 //APP CONFIG
 mongoose.connect("mongodb://localhost:27017/restful_blog_app", { useNewUrlParser: true });
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(expressSanitizer());
 app.use(methodOverride("_method"));
 
 //MONGOOSE/MODEL CONFIG
@@ -46,9 +48,14 @@ app.get("/blogs", function(req, res){
 app.get("/blogs/new", function(req, res){
     res.render("new");
 });
+
 //CREATE ROUTE
 app.post("/blogs", function(req, res){
     //create blog
+    console.log(req.body);
+    req.body.blog.body = req.sanitize(req.body.blog.body)
+    console.log("=============")
+    console.log(req.body);
     Blog.create(req.body.blog, function(err, newBlog){
         if(err){
             res.render("new");
@@ -83,6 +90,7 @@ app.get("/blogs/:id/edit", function(req, res){
 
 //UPDATE ROUTE
 app.put("/blogs/:id", function(req, res){
+    req.body.blog.body = req.sanitize(req.body.blog.body)
     Blog.findByIdAndUpdate(req.params.id, req.body.blog, function(err, updatedBlog){
         if(err){
             res.redirect("/blogs");
@@ -94,7 +102,6 @@ app.put("/blogs/:id", function(req, res){
 
 //DELETE ROUTE
 app.delete("/blogs/:id", function(req, res){
-    res.send("Destroy Rote");
     //destroy blog
     Blog.findByIdAndRemove(req.params.id, function(err){
         if(err){
