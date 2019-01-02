b56d65de9e1a10bb16041732bbc0b05b27d4c075
Linda Allen <lindaallenx@gmail.com>
2019-01-02 16:55:38 +0000

    add delete route

--- app.js
+++ app.js
@@ -92,6 +92,20 @@ app.put("/blogs/:id", function(req, res){
     });
 });
 
+//DELETE ROUTE
+app.delete("/blogs/:id", function(req, res){
+    res.send("Destroy Rote");
+    //destroy blog
+    Blog.findByIdAndRemove(req.params.id, function(err){
+        if(err){
+            res.redirect("/blogs");
+        } else {
+            res.redirect("/blogs");
+        }
+    })
+    //redirect somewhere
+});
+
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("Server is running");
 })
